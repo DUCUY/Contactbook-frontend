2ca79d5e69a58c2a11a1b226f1ee7ba61882a856
TranDucUy <uyb1910174@student.ctu.edu.vn>
2022-11-02 23:16:04 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <h1>Hello, vue.js!</h1>
-  
+  <!-- <h1>Hello, vue.js!</h1> -->
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
+//import dt1 from "../services/contact.service.js"
+import AppHeader from "@/components/AppHeader.vue";
 export default {
-
-}
+  // mounted(){
+  // dt1.getAll();
+  //  }
+  components: {
+    AppHeader,
+  },
+};
 </script>
 <style>
-  .page{
-    max-width: 400px;
-    margin: auto;
-  }
-
+.page {
+  max-width: 400px;
+  margin: auto;
+}
 </style>
